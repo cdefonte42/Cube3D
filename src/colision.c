94e5c20e8103231f0a57b7a69b54efc45844ac03
Cyrielle DEFONTE <cyrielledefonte@gmail.com>
2022-07-23 18:50:15 +0200

    FIX: colision ok (oublis la convertion de map a grid unit...) MAIS reste la colision si marche arriere

--- src/colision.c
+++ src/colision.c
@@ -6,7 +6,7 @@
 /*   By: Cyrielle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 14:00:03 by Cyrielle          #+#    #+#             */
-/*   Updated: 2022/07/23 14:19:48 by Cyrielle         ###   ########.fr       */
+/*   Updated: 2022/07/23 18:29:56 by Cyrielle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 /* Si la distance entre le mur et le player est inferieure a la distance a 
 parcourir, alors il y a colision. */
+// PLUS OU MOINS OK puisque collision en marche arriere pas faite....
 bool	check_for_colision(t_game *game, int keycode)
 {
 	t_ray	mid_ray;
 
 	mid_ray = game->player.rays[(game->width / 2) - 1];
 
-	if (keycode == UP_ARW && mid_ray.hit_point.dist <= game->player.mv_speed)
+	if (keycode == UP_ARW && mid_ray.hit_point.dist <= game->player.mv_speed * 64.0)
 		return (true);
 	return (false);
 }
