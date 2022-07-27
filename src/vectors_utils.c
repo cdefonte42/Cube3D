4254c808d2cae90f99caf0382440cc3d2b9a6d43
Cyrielle DEFONTE <cyrielledefonte@gmail.com>
2022-07-27 19:56:56 +0200

    style: norminette

--- src/vectors_utils.c
+++ src/vectors_utils.c
@@ -6,19 +6,20 @@
 /*   By: cdefonte <cdefonte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:09:58 by cdefonte          #+#    #+#             */
-/*   Updated: 2022/07/27 18:38:56 by Cyrielle         ###   ########.fr       */
+/*   Updated: 2022/07/27 19:02:06 by Cyrielle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
 
-/* Matrice de rotation: x' = cos(alpha).x - sin(alpha).y et y' = sin(alpha).x + cos(alpha).y */
+/* Matrice de rotation: x' = cos(alpha).x - sin(alpha).y et 
+y' = sin(alpha).x + cos(alpha).y */
 struct s_coord	rotate_vector(struct s_coord from, double angle)
 {
 	struct s_coord	res;
 
 	res.x = cos(angle) * from.x - sin(angle) * from.y;
 	res.y = sin(angle) * from.x + cos(angle) * from.y;
-	res.z = CUBE_SIZE / 2;	// useless for now
+	res.z = CUBE_SIZE / 2;
 	return (res);
 }
