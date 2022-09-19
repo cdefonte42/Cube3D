8caa386fd284892773f67c4fa94d0b654a9c460c
Hoppy <mbraets@student.42.fr>
2022-09-19 12:22:59 +0200

    Norm - init file minimal

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 14:13:54 by Cyrielle          #+#    #+#             */
-/*   Updated: 2022/09/15 17:50:38 by mbraets          ###   ########.fr       */
+/*   Updated: 2022/09/19 12:22:44 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	init_minimap(t_game *game)
 {
-	if (game->map.img.height > game->height * 0.25 || game->map.img.width > game->width * 0.25)
+	if (game->map.img.height > game->height * 0.25 || \
+	game->map.img.width > game->width * 0.25)
 	{
 		game->minimap.height = game->height * 0.25;
-		game->minimap.width = game->minimap.height;
-		game->minimap.ptr = mlx_new_image(game->mlx_ptr, game->minimap.width, game->minimap.height);
+			game->minimap.width = game->minimap.height;
+		game->minimap.ptr = mlx_new_image(game->mlx_ptr, game->minimap.width, \
+			game->minimap.height);
 		if (!game->minimap.ptr)
 			return (-1);
 		game->minimap.data = (int *)mlx_get_data_addr(game->minimap.ptr, \
@@ -32,12 +34,11 @@ int	init_minimap(t_game *game)
 
 int	init_map(t_game *game, int argc, char **argv)
 {
-	int		nb_pixelX;
-	int		nb_pixelY;
+	int		nb_pixel_x;
+	int		nb_pixel_y;
 
 	if (argc != 2)
 		return (error("Invalid number of arguments", NULL), -1);
-	
 	game->text = ft_calloc(sizeof(t_texture), 4);
 	if (!map_parsing(game, argv[1]))
 		return (-1);
@@ -47,24 +48,24 @@ int	init_map(t_game *game, int argc, char **argv)
 	game->map.state = off;
 	game->map.rcube_size = 16;
 	game->map.ratio = (double)game->map.rcube_size / (double)game->cube_size;
-	nb_pixelX = game->map.width * game->map.rcube_size + 1;
-	nb_pixelY = game->map.height * game->map.rcube_size + 1;
-	game->map.grid.ptr = mlx_new_image(game->mlx_ptr, nb_pixelX, nb_pixelY);
+	nb_pixel_x = game->map.width * game->map.rcube_size + 1;
+	nb_pixel_y = game->map.height * game->map.rcube_size + 1;
+	game->map.grid.ptr = mlx_new_image(game->mlx_ptr, nb_pixel_x, nb_pixel_y);
 	if (!game->map.grid.ptr)
 		return (-1);
-	game->map.img.ptr = mlx_new_image(game->mlx_ptr, nb_pixelX, nb_pixelY);
+	game->map.img.ptr = mlx_new_image(game->mlx_ptr, nb_pixel_x, nb_pixel_y);
 	if (!game->map.img.ptr)
 		return (-1);
 	game->map.grid.data = (int *)mlx_get_data_addr(game->map.grid.ptr, \
 	&game->map.grid.bpp, &game->map.grid.size_line, &game->map.grid.endian);
 	game->map.grid.size_line /= 4;
-	game->map.grid.height = nb_pixelY;
-	game->map.grid.width = nb_pixelX;
+	game->map.grid.height = nb_pixel_y;
+	game->map.grid.width = nb_pixel_x;
 	game->map.img.data = (int *)mlx_get_data_addr(game->map.img.ptr, \
 	&game->map.img.bpp, &game->map.img.size_line, &game->map.img.endian);
 	game->map.img.size_line /= 4;
-	game->map.img.height = nb_pixelY;
-	game->map.img.width = nb_pixelX;
+	game->map.img.height = nb_pixel_y;
+	game->map.img.width = nb_pixel_x;
 	draw_walls(game);
 	return (0);
 }
@@ -153,7 +154,7 @@ int	init_game(t_game *game, int argc, char **argv)
 	if (!game->img.ptr)
 		return (-1);
 	game->img.data = (int *)mlx_get_data_addr(game->img.ptr, &game->img.bpp, \
-	&game->img.size_line, &game->img.endian);
+		&game->img.size_line, &game->img.endian);
 	game->img.size_line /= 4;
 	game->img.width = SCREEN_W;
 	game->img.height = SCREEN_H;
